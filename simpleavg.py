1f8f6f14051fa801a04da49ba73e9ee2fb7e4974
Ian McEwen <mian@cyverse.org>
2021-12-05 23:25:23 -0700

    more calculation

--- simpleavg.py
+++ simpleavg.py
@@ -1,26 +1,27 @@
 import util
 import copy
 
-def points(place, default=1):
-    placeranks = [150,120,100,90,80,70,60,50,40,30,20,10,9,8,7,6,5,4,3,2,1]
+def points(races, place, default=1):
+    playernum = len(util.all_players(races))
+    placeranks = list(range(playernum,0,-1))
     if place < len(placeranks):
         return placeranks[place]
     else:
         return default
 
-def racepoints(player, race):
+def racepoints(player, race, races):
     if isinstance(race["finishers"], list):
         for i in range(len(race["finishers"])):
             if player == race["finishers"][i]:
-                return points(i)
+                return points(races,i)
     elif isinstance(race["finishers"], dict):
         for k in sorted(race["finishers"].keys()):
             if isinstance(race["finishers"][k], list):
                 for p in race["finishers"][k]:
                     if p == player:
-                        return points(k-1)
+                        return points(races,k-1)
             else:
-                return points(k-1)
+                return points(races,k-1)
     elif player in race["forfeits"]:
         return 0
     else:
@@ -28,14 +29,14 @@ def racepoints(player, race):
 
 def raceranks(races, startval=0):
     ranks = [dict([(p, startval) for p in util.all_players(races)])] + [None for r in range(len(races))]
-    ranks[1] = dict([(p, racepoints(p, races[0]) or startval) for p in util.all_players(races[:1])])
+    ranks[1] = dict([(p, racepoints(p, races[0], races) or startval) for p in util.all_players(races[:1])])
     for r in range(1,len(races)):
         race = races[r]
         ranks[r+1] = copy.deepcopy(ranks[r])
         for player in util.all_players([race]):
-            points = [racepoints(player, race) for race in races[:r] if racepoints(player, race) is not None]
-            if racepoints(player, race) is not None:
-                points = points + [racepoints(player, race)]
+            points = [racepoints(player, race, races) for race in races[:r] if racepoints(player, race, races) is not None]
+            if racepoints(player, race, races) is not None:
+                points = points + [racepoints(player, race, races)]
             if len(points) > 0:
                 ranks[r+1][player] = sum(points) / len(points)
             else:
