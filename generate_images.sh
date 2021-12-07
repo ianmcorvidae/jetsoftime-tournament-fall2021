bb173ad0162cb12a0f735801610b342bdec30f37
Ian McEwen <mian@cyverse.org>
2021-12-06 20:28:42 -0700

    Only include complete races in generated images

--- generate_images.sh
+++ generate_images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for ltype in elo elohighknum eloten ff6wc mariokart mariokartavg simpleavg raster rasteradjusted raster1500 rasterfantasy;
-do python3 cmdline.py $ltype --plot --output-file img/${ltype}.png;
+do python3 cmdline.py $ltype --plot --no-incomplete --output-file img/${ltype}.png;
 done;
