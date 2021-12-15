9240028012d663dc69e14b151a1c29f5d7e656ce
Ian McEwen <mian@cyverse.org>
2021-12-14 19:37:32 -0700

    Change images to linewidth of 1.5

--- graph.py
+++ graph.py
@@ -10,7 +10,7 @@ def graph(races, ranks, startval=0, title="Ranks", exclude=[], output_file=None)
     for player in [x[1] for x in util.byscore(ranks[-1]) if x[1] not in exclude]:
         x = range(len(ranks))
         y = [r.get(player, startval) for r in ranks]
-        plt.plot(x, y, label=player)
+        plt.plot(x, y, label=player, lw=1.5)
     plt.legend(loc='best', frameon=False)
     plt.title(title)
     if output_file is None:
